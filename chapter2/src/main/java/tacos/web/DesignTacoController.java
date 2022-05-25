1146fb08c5c46730c2e61e0f20cfdd20f3b013ca
yimizhao <zhao12051610@163.com>
2022-05-25 17:25:25 +0800

    添加转换器<IngredientByIdConverter>

--- chapter2/src/main/java/tacos/web/DesignTacoController.java
+++ chapter2/src/main/java/tacos/web/DesignTacoController.java
@@ -3,10 +3,7 @@ package tacos.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import tacos.Ingredient;
 import tacos.Ingredient.*;
 import tacos.Taco;
@@ -62,5 +59,12 @@ public class DesignTacoController {
     @ModelAttribute(name = "tacoOrder")
     public TacoOrder order() {
         return new TacoOrder();
+    }
+
+    @PostMapping
+    public String processTaco(Taco taco,@ModelAttribute TacoOrder tacoOrder) {
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}", taco);
+        return "redirect:/orders/current";
     }
 }
