bed1db0c307fd8c6713632b204bd5bb6f68b9705
YimiZhao <zhao12051610@163.com>
2022-05-28 21:27:55 +0800

    利用WebMvcConfigurer定义简单的controller

--- chapter2/src/main/java/tacos/Chapter2Application.java
+++ chapter2/src/main/java/tacos/Chapter2Application.java
@@ -5,9 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Chapter2Application {
-
+/*    public class Chapter2Application implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }*/
     public static void main(String[] args) {
         SpringApplication.run(Chapter2Application.class, args);
     }
+
 
 }
