f5ed93f4eb145c5517f23c2a49033be55e437857
yimizhao <zhao12051610@163.com>
2022-06-02 16:21:39 +0800

    mongoDB模块初始化

---
+++ chapter4_SpringDataMongoDB/src/main/java/tacos/Chapter4SpringDataMongoDbApplication.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter4SpringDataMongoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter4SpringDataMongoDbApplication.class, args);
+    }
+
+}
---
+++ chapter4_SpringDataMongoDB/src/main/java/tacos/Ingredient.java
@@ -0,0 +1,29 @@
+package tacos;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Persistable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PUBLIC, force = true)
+public class Ingredient {
+    @Id
+    private String id;
+
+    private String name;
+
+    private Type type;
+
+
+
+    public enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}
