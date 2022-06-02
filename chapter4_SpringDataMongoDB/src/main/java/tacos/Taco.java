921a3d43709174a08b0003fbe7344c64a6c256ff
YimiZhao <zhao12051610@163.com>
2022-06-02 20:43:56 +0800

    springboot2.7.0 mangoDB 异常

--- chapter4_SpringDataMongoDB/src/main/java/tacos/Taco.java
+++ chapter4_SpringDataMongoDB/src/main/java/tacos/Taco.java
@@ -1,20 +1,22 @@
 package tacos;
 
-import lombok.Data;
-
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+
 @Data
 public class Taco {
 
-    private Date createAt = new Date();
     @NotNull
     @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
+
+    private Date createdAt = new Date();
 
     @Size(min = 1, message = "You must choose at least 1 ingredient")
     private List<Ingredient> ingredients = new ArrayList<>();
@@ -22,4 +24,5 @@ public class Taco {
     public void addIngredient(Ingredient ingredient) {
         this.ingredients.add(ingredient);
     }
+
 }
--- chapter4_SpringDataMongoDB/src/main/java/tacos/data/OrderRepository.java
+++ chapter4_SpringDataMongoDB/src/main/java/tacos/data/OrderRepository.java
@@ -1,8 +1,10 @@
 package tacos.data;
 
 import org.springframework.data.repository.CrudRepository;
+
 import tacos.TacoOrder;
 
-public interface OrderRepository extends CrudRepository<TacoOrder,String> {
+public interface OrderRepository
+        extends CrudRepository<TacoOrder, String> {
 
 }
